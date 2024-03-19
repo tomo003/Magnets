0a08af493d42752774add0c07c62b9ad9b833e7d
Tomoya Anazawa <zeal22300004@fsgcl.jp>
2024-03-19 11:45:27 +0900

    プロジェクト ファイルを追加します。

---
+++ GameSources/GameStage.cpp
@@ -0,0 +1,42 @@
+/*!
+@file GameStage.cpp
+@brief ゲームステージ実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	//	ゲームステージクラス実体
+	//--------------------------------------------------------------------------------------
+	void GameStage::CreateViewLight() {
+		const Vec3 eye(0.0f, 5.0f, -5.0f);
+		const Vec3 at(0.0f);
+		auto PtrView = CreateView<SingleView>();
+		//ビューのカメラの設定
+		auto PtrCamera = ObjectFactory::Create<Camera>();
+		PtrView->SetCamera(PtrCamera);
+		PtrCamera->SetEye(eye);
+		PtrCamera->SetAt(at);
+		//マルチライトの作成
+		auto PtrMultiLight = CreateLight<MultiLight>();
+		//デフォルトのライティングを指定
+		PtrMultiLight->SetDefaultLighting();
+	}
+
+
+
+	void GameStage::OnCreate() {
+		try {
+			//ビューとライトの作成
+			CreateViewLight();
+		}
+		catch (...) {
+			throw;
+		}
+	}
+
+}
+//end basecross
