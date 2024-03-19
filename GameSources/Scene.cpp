da133ffda63c1152f8099a17ab930cab578b3d79
Rin Yoshida <zeal22300024@fsgcl.jp>
2024-03-19 15:02:38 +0900

    プレイヤーオブジェクトを追加した

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -9,11 +9,32 @@
 
 namespace basecross{
 
+	void Scene::CreateResourses() {
+		//アプリケーションオブジェクトへの参照を取得する
+		auto& app = App::GetApp();
+
+		//テクスチャが入っているフォルダのパスを文字列として取得する
+		auto mediaPath = app->GetDataDirWString();//「media」パスを文字列として取得する
+		auto texturePath = mediaPath + L"Texture/";//「Textures」フォルダのパスを連結する
+
+		wstring strTexture = texturePath + L"pcube01.png";
+		app->RegisterTexture(L"Player_TX", strTexture);
+
+
+		//モデルが入っているフォルダのパスを文字列として取得する
+		auto modelPath = mediaPath + L"Model/";//「Model」フォルダのパスを連結する
+
+		auto staticPlayerMesh = MeshResource::CreateStaticModelMesh(modelPath, L"01.bmf");
+		App::GetApp()->RegisterResource(L"Player_MESH", staticPlayerMesh);
+	}
+
 	//--------------------------------------------------------------------------------------
 	///	ゲームシーン
 	//--------------------------------------------------------------------------------------
 	void Scene::OnCreate(){
 		try {
+			CreateResourses();
+
 			//クリアする色を設定
 			Col4 Col;
 			Col.set(31.0f / 255.0f, 30.0f / 255.0f, 71.0f / 255.0f, 255.0f / 255.0f);
