b2bb5cafefa42fe9bcb103580c200d58b65ce8b3
Rin Yoshida <zeal22300024@fsgcl.jp>
2024-03-19 15:01:36 +0900

    地面を追加した

---
+++ GameSources/Ground.cpp
@@ -0,0 +1,26 @@
+/*!
+@file Ground.cpp
+@brief 地面を表すクラスの実装
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	void Ground::OnCreate()
+	{
+		auto drawComp = AddComponent<PNTStaticDraw>();
+		drawComp->SetMeshResource(L"DEFAULT_CUBE");
+		drawComp->SetDiffuse(Col4(1.0f, 0.1f, 0.1f, 1.0f));
+
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+
+		auto transComp = GetComponent<Transform>();
+		transComp->SetPosition(0.0f, -1.5f, 0.0f);
+		transComp->SetScale(5.0f, 1.0f, 1.0f);
+
+
+	}
+}
+//end basecross
--- GameSources/Ground.h
+++ GameSources/Ground.h
@@ -1,1 +1,21 @@
+/*!
+@file Ground.h
+@brief 地面を表すクラスを定義する
+*/
+
 #pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class Ground : public GameObject
+	{
+	public:
+		Ground(const std::shared_ptr<Stage>& stage) :
+			GameObject(stage)
+		{
+		}
+
+		void OnCreate();
+	};
+}
+//end basecross
