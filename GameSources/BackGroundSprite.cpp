5dc2bf0e7e624dabefe8a34a1c6093a6c59c1eb2
yuu2611 <zeal22300025@fsgcl.jp>
2024-04-11 10:39:54 +0900

    背景処理の修正

--- GameSources/BackGroundSprite.cpp
+++ GameSources/BackGroundSprite.cpp
@@ -9,16 +9,17 @@
 
 namespace basecross {
 	void BackGroundSprite::OnCreate(){
-		m_mesh = MeshResource::CreateCube(1.0f, true);
+		m_mesh = MeshResource::CreateCube(5.0f, true);
 
 		auto drawComp = AddComponent<PNTStaticDraw>();
+		drawComp->SetMeshResource(m_mesh);
 		drawComp->SetTextureResource(L"BACKGROUND");
 		drawComp->SetSamplerState(SamplerState::AnisotropicWrap);
 
-		const Vec3 scale(100.0f, 50.0f, 1.0f);
+		const Vec3 scale(200.00f, 30.0f, 1.0f);
 		auto transComp = AddComponent<Transform>();
 		transComp->SetScale(scale);
-		transComp->SetPosition(Vec3(0.0f, 0.0f, 0.0f));
+		transComp->SetPosition(Vec3(0.0f, 5.0f, 3.0f));
 
 		auto vertices = m_mesh->GetBackupVerteces<VertexPositionNormalTexture>();
 		for (auto& vertex : vertices) { 
@@ -37,11 +38,5 @@ namespace basecross {
 		}
 
 		m_mesh->UpdateVirtexBuffer<VertexPositionNormalTexture>(vertices);
-		//Sprites::CreateSprite(Vec3(-m_DefaultSize.x / 2, m_DefaultSize.y / 2, 10.0f), m_DefaultSize, L"BACKGROUND");
-
-	}
-
-	void BackGroundSprite::OnUpdate() {
-		Sprites::OnUpdate();
 	}
 }
--- GameSources/BackGroundSprite.h
+++ GameSources/BackGroundSprite.h
@@ -8,19 +8,18 @@
 #include "Sprites.h"
 
 namespace basecross {
-	class BackGroundSprite : public Sprites {
+	class BackGroundSprite : public GameObject {
 		shared_ptr<MeshResource> m_mesh;
 		const Vec2 m_DefaultSize; // 初期サイズ
 
 	public:
 		// コンストラクタ
 		BackGroundSprite(const shared_ptr<Stage>& stage) :
-			Sprites(stage),
+			GameObject(stage),
 			m_DefaultSize(Vec2((float)App::GetApp()->GetGameWidth(), (float)App::GetApp()->GetGameHeight()))
 		{
 		}
 
 		void OnCreate() override; // 初期化
-		void OnUpdate() override;
 	};
 }
