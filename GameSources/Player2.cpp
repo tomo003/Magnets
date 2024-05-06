da6c9bb55ff0670d782482a20f10a5e9313e9a93
Tomoya Anazawa <zeal22300004@fsgcl.jp>
2024-05-06 10:53:52 +0900

    あ

--- GameSources/Player2.cpp
+++ GameSources/Player2.cpp
@@ -167,7 +167,7 @@ namespace basecross {
 	//リスポーンする
 	void Player2::RespawnPlayer(float respawnPoint) {
 		m_pos = Vec3(respawnPoint, 0.0f, 0.0f);
-		m_ptrTrans->SetPosition(m_pos);
+		m_ptrTrans->SetPosition(Vec3(m_pos));
 	}
 
 	//アニメーション関数
