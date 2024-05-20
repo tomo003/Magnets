8f1c8be91dec6ace6062212b4e29a78dec0f167a
yuu2611 <zeal22300025@fsgcl.jp>
2024-05-20 09:44:08 +0900

    エフェクトの移動の修正

--- GameSources/MoveMagnetsObjct.cpp
+++ GameSources/MoveMagnetsObjct.cpp
@@ -51,22 +51,7 @@ namespace basecross {
 		//m_ptrArea->UpdateArea(m_position);
 
 		Vec3 position = m_ptrTrans->GetPosition();
-		
-		Vec3 startPos = m_points[0];
-		Vec3 endPos = m_points[1];
-		if (position.x >= startPos.x ) {
-			isEfkMove = true;
-		}
-		else if (position.x <= endPos.x ) {
-			isEfkMove = false;
-		}
-		if (isEfkMove) {
-			m_efk->AddLocation(endPos * delta * -0.031f);
-		}
-		else if(!isEfkMove){
-			m_efk->AddLocation(startPos * delta * 0.031f);
-		}
-		
+		m_efk->SetLocation(position);
 	}
 
 	// プレイヤーに磁力による力を適用
