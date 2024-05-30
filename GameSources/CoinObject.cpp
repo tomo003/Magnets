2802f899a9d7a48377e4ddf23d9ca7dba26a30eb
yuu2611 <zeal22300025@fsgcl.jp>
2024-05-30 13:58:01 +0900

    コインを取得できるように変更

--- GameSources/CoinObject.cpp
+++ GameSources/CoinObject.cpp
@@ -9,26 +9,36 @@
 namespace basecross {
 
 	void CoinObject::OnCreate() {
-		auto ptrTransform = GetComponent<Transform>();
-		ptrTransform->SetScale(Vec3(1.0f));
-		ptrTransform->SetPosition(m_Position);
+		auto ptrTrans = AddComponent<Transform>();
+		ptrTrans->SetScale(Vec3(0.8f, 0.8f,0.1f));
+		ptrTrans->SetPosition(m_position);
+		auto ptrDraw = AddComponent<PNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_SPHERE");
 		auto ptrColl = AddComponent<CollisionObb>();
 		ptrColl->SetUpdateActive(true);
-		ptrColl->SetFixed(true);
-		//ptrColl->SetDrawActive(true);
+		ptrColl->SetDrawActive(true);
 
 		AddTag(L"CoinObject");
 
 		Mat4x4 spanMat;
 		spanMat.affineTransformation(
-			Vec3(1.0f, 1.0f, 1.0f),
+			Vec3(1.5f, 1.5f, 1.5f),
 			Vec3(0.0f, 0.0f, 0.0f),
 			Vec3(0.0f, 0.0f, 0.0f),
-			Vec3(0.0f, -0.3f, 0.0f)
+			Vec3(0.0f, 0.0f, 0.0f)
 		);
 	}
 
 	void CoinObject::OnUpdate() {
+	}
 
+	void CoinObject::OnCollisionEnter(shared_ptr<GameObject>& Other) {
+		auto ptrPlayer = dynamic_pointer_cast<Player>(Other);
+		auto ptrPlayer2 = dynamic_pointer_cast<Player2>(Other);
+
+		if (ptrPlayer || ptrPlayer2) {
+			SetUpdateActive(false);
+			SetDrawActive(false);
+		}
 	}
 }
--- GameSources/CoinObject.h
+++ GameSources/CoinObject.h
@@ -9,18 +9,19 @@
 namespace basecross {
 
 	class CoinObject : public GameObject {
-		Vec3 m_Position;
+		Vec3 m_position;
 	public:
 		//ç\ízÇ∆îjä¸
-		CoinObject(const shared_ptr<Stage>& StagePtr, const Vec3& Position) :
+		CoinObject(const shared_ptr<Stage>& StagePtr, const Vec3& position) :
 			GameObject(StagePtr),
-			m_Position(Position)
+			m_position(position)
 		{
 		}
 		virtual ~CoinObject() {};
 
 		virtual void OnCreate() override;
 		virtual void OnUpdate() override;
+		void OnCollisionEnter(shared_ptr<GameObject>& Other);
 
 	};
 }
