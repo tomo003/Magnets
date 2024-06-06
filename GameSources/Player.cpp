b3689da3a44e42e1f021118cd4551a42877da3be
Rin Yoshida <zeal22300024@fsgcl.jp>
2024-06-06 14:32:38 +0900

    磁石のN,Sにくっついたときにエフェクトが連続で出るのをなおした

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -162,7 +162,7 @@ namespace basecross {
 		}
 
 		if (!isEffect) {
-			if (length(m_objPos - m_pos) >= 2.0f) {
+			if (length(m_objPos - m_pos) >= 1.5f) {
 				isEffect = true;
 			}
 		}
@@ -422,6 +422,7 @@ namespace basecross {
 			if (isEffect) {
 				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
 				isEffect = false;
+				m_objPos = ptrMoveMetal->GetComponent<Transform>()->GetPosition();
 				auto XAPtr = App::GetApp()->GetXAudio2Manager();
 				XAPtr->Start(L"UNION_SE", 0, 2.0f);
 			}
@@ -442,19 +443,25 @@ namespace basecross {
 		if (ptrMagnetN && (m_eMagPole == EState::eS)) {
 			m_gravityComp->SetGravityZero();
 			m_ptrTrans->SetParent(ptrMagnetN);
-			GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
-			isEffect = true;
-			auto XAPtr = App::GetApp()->GetXAudio2Manager();
-			XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			if (isEffect) {
+				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
+				isEffect = false;
+				m_objPos = ptrMagnetN->GetComponent<Transform>()->GetPosition();
+				auto XAPtr = App::GetApp()->GetXAudio2Manager();
+				XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			}
 			m_emState = magneticState::emMagnet;
 		}
 		if (ptrMagnetS && (m_eMagPole == EState::eN)) {
 			m_gravityComp->SetGravityZero();
 			m_ptrTrans->SetParent(ptrMagnetS);
-			GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
-			isEffect = true;
-			auto XAPtr = App::GetApp()->GetXAudio2Manager();
-			XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			if (isEffect) {
+				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
+				isEffect = false;
+				m_objPos = ptrMagnetS->GetComponent<Transform>()->GetPosition();
+				auto XAPtr = App::GetApp()->GetXAudio2Manager();
+				XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			}
 			m_emState = magneticState::emMagnet;
 		}
 		if (ptrRing && (m_eMagPole != EState::eFalse)) {
@@ -497,7 +504,7 @@ namespace basecross {
 		}
 		if (ptrGround) {
 			//isGround = true;
-			isEffect = true;
+			//isEffect = true;
 			isInertia = false;
 			isRepulsion = false;
 		}
--- GameSources/Player2.cpp
+++ GameSources/Player2.cpp
@@ -152,7 +152,7 @@ namespace basecross {
 		}
 
 		if (!isEffect) {
-			if (length(m_objPos - m_pos) >= 2.0f) {
+			if (length(m_objPos - m_pos) >= 1.5f) {
 				isEffect = true;
 			}
 		}
@@ -396,6 +396,7 @@ namespace basecross {
 			if (isEffect) {
 				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
 				isEffect = false;
+				m_objPos = ptrMoveMetal->GetComponent<Transform>()->GetPosition();
 				auto XAPtr = App::GetApp()->GetXAudio2Manager();
 				XAPtr->Start(L"UNION_SE", 0, 2.0f);
 			}
@@ -414,19 +415,24 @@ namespace basecross {
 		if (ptrMagnetN && (m_eMagPole == EState::eS)) {
 			m_gravityComp->SetGravityZero();
 			m_ptrTrans->SetParent(ptrMagnetN);
-			GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
-			isEffect = true;
-			auto XAPtr = App::GetApp()->GetXAudio2Manager();
-			XAPtr->Start(L"UNION_SE", 0, 2.0f);
-
+			if (isEffect) {
+				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
+				isEffect = false;
+				m_objPos = ptrMagnetN->GetComponent<Transform>()->GetPosition();
+				auto XAPtr = App::GetApp()->GetXAudio2Manager();
+				XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			}
 		}
 		if (ptrMagnetS && (m_eMagPole == EState::eN)) {
 			m_gravityComp->SetGravityZero();
 			m_ptrTrans->SetParent(ptrMagnetS);
-			GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
-			isEffect = true;
-			auto XAPtr = App::GetApp()->GetXAudio2Manager();
-			XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			if (isEffect) {
+				GetStage()->AddGameObject<EffectPlayer>(m_pos, Vec3(1.0f), L"impact");
+				isEffect = false;
+				m_objPos = ptrMagnetS->GetComponent<Transform>()->GetPosition();
+				auto XAPtr = App::GetApp()->GetXAudio2Manager();
+				XAPtr->Start(L"UNION_SE", 0, 2.0f);
+			}
 		}
 		if (ptrPlayer) {
 		isPlayerContact = true;
@@ -445,7 +451,7 @@ namespace basecross {
 		}
 
 		if (ptrGround) {
-			isEffect = true;
+			//isEffect = true;
 			isGround = true;
 			isInertia = false;
 			isRepulsion = false;
